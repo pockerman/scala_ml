99f3865e74869b4b2ddb235747c7c6074ee97ccc
pockerman <a.giavaras@gmail.com>
2020-07-07 22:05:59 +0100

    First commit for engine main package

---
+++ src/main/scala/engine/LinearRegression.scala
@@ -0,0 +1,14 @@
+package engine
+
+import breeze.linalg._
+
+/// Linear regression class
+class LinearRegression {
+
+
+  
+  def fit(x: DenseMatrix[Double], y: DenseVector[Double]):Unit={
+
+  }
+
+}
