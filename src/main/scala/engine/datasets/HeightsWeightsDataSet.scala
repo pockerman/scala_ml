61707275c82dfc56e863c9e3b8c77c576970a839
Alex <a.giavaras@gmail.com>
2020-12-29 15:17:05 +0000

    Add HeightsWeightsDataSet class

---
+++ src/main/scala/engine/datasets/HeightsWeightsDataSet.scala
@@ -0,0 +1,34 @@
+package engine.datasets
+
+import breeze.linalg.DenseVector
+
+/**
+ * Wrapper for the heights weights dataset
+ * @param weights
+ * @param heights
+ * @param reportedWeights
+ * @param reportedHeights
+ * @param genders
+ */
+class HeightsWeightsDataSet (val weights:DenseVector[Double], val heights:DenseVector[Double],
+                             val reportedWeights:DenseVector[Double], val reportedHeights:DenseVector[Double],
+                              val genders:DenseVector[Char]){
+
+  val npoints: Int = heights.length;
+
+  require(weights.length == npoints)
+  require(reportedWeights.length == npoints)
+  require(genders.length == npoints)
+  require(reportedHeights.length == npoints)
+
+  val weightsCol = weights;
+  val heightsCol = heights;
+  val reportedWeightsCol = reportedWeights;
+  val reportedHeightsCol = reportedHeights;
+  val gendersCol = genders;
+
+  val filename = "data/rep_height_weights.csv"
+
+
+
+}
