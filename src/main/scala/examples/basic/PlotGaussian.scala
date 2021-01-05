4aca896210f0b4ead0c52a687564d4b42f1223d7
Alex <a.giavaras@gmail.com>
2021-01-05 15:38:48 +0000

    Add plot gaussian example

---
+++ src/main/scala/examples/basic/PlotGaussian.scala
@@ -0,0 +1,22 @@
+package examples.basic
+
+import breeze.linalg._
+import breeze.plot._
+import engine.utils.Constants
+
+object PlotGaussian {
+
+  def main(args: Array[String]):Unit={
+
+    // create a figure
+    val f = Figure()
+
+    val p = f.subplot(0)
+    val g = breeze.stats.distributions.Gaussian(0,1)
+
+    p += hist(g.sample(100000),100)
+    p.title = "A normal distribution"
+    f.saveas(Constants.defaultOutputDir + "subplots.png")
+  }
+
+}
