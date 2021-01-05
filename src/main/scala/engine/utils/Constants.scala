e656f265ab3f7f25ac0e8c29d47a83a2ca8fa110
Alex <a.giavaras@gmail.com>
2021-01-05 15:37:07 +0000

    Add Constants class

---
+++ src/main/scala/engine/utils/Constants.scala
@@ -0,0 +1,16 @@
+package engine.utils
+
+object Constants {
+
+  /**
+   * Default output directory
+   */
+  val defaultOutputDir: String = "/home/alex/qi3/scalarl/output/"
+
+  /**
+   * Default data directory
+   */
+  val defaultDataDirectory: String = "/home/alex/qi3/scalarl/data"
+
+
+}
