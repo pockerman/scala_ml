4eae1a4d9e8c998b1664fbb87852bb6b7e3934ea
Alex <a.giavaras@gmail.com>
2021-01-13 14:33:20 +0000

    Add basic optimize example

---
+++ src/main/scala/examples/basic/Optimize.scala
@@ -0,0 +1,18 @@
+package examples.basic
+
+import breeze.linalg._
+import breeze.optimize._
+
+object Optimize {
+
+  def main(args: Array[String]):Unit={
+
+    val f = new DiffFunction[DenseVector[Double]] {
+      def calculate(x: DenseVector[Double])={
+        (norm((x-3d) :^ 2d,1d),(x * 2d) -6d);
+      }
+    }
+
+  }
+
+}
