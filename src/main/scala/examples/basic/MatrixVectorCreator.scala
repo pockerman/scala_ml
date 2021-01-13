128c7d4a633eb10effaa1573a012d66fac942f56
Alex <a.giavaras@gmail.com>
2021-01-13 14:32:51 +0000

    Add basic MatrixVector excample

---
+++ src/main/scala/examples/basic/MatrixVectorCreator.scala
@@ -0,0 +1,37 @@
+package examples.basic
+
+import breeze.linalg._
+
+object MatrixVectorCreator {
+
+  def main(args: Array[String]):Unit={
+
+    System.out.println("Creating DenseVector...")
+
+    // create a vector of size 5
+    val x = DenseVector.zeros[Double](5);
+
+    // we can access the vector elements using
+    // indices from 0 to c.length-1
+    System.out.println("The 0-th vector element is="+x(0))
+
+    // change the i-th element
+    val i = 1
+    x(i) = 5;
+
+    System.out.println("The "+i+"-th vector element is="+x(i))
+
+    System.out.println("Creating DenseMatrix...")
+
+    val m = DenseMatrix.zeros[Double](5, 5)
+
+    System.out.println("Number of rows="+m.rows)
+    System.out.println("Number of columns="+m.cols)
+
+    // change the last row
+    m(4, ::) := DenseVector(1.,2.,3.,4.,5.).t
+  }
+
+
+
+}
