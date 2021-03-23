c6b76db12e6bfb9539817b08339e0129867d9fe8
Alex <a.giavaras@gmail.com>
2021-03-23 17:02:21 +0000

    Add LossFunction and ScalarFunction class

---
+++ src/main/scala/engine/maths/functions/LossFunction.scala
@@ -0,0 +1,38 @@
+package engine.maths.functions
+
+import breeze.linalg.DenseVector
+
+/**
+ * A wrapper class for ErrorFunction and
+ * Function objects
+ */
+class LossFunction(hypothesisFunction: ScalarFunction, errorFunction: ErrorFunction) {
+
+  /**
+   * The monomials modeling the Polynomial
+   */
+  var hFunction = hypothesisFunction
+
+  /**
+   * The maximum degree of the polynomial
+   */
+  var errFunction = errorFunction
+
+  /**
+   * Compute the value of the Loss function
+   */
+  def value(y: Double, x: Double): Double = {
+
+    var hval = hFunction.value(x)
+    var vecY = DenseVector[Double](y);
+    var vecX = DenseVector[Double](hval);
+    var result = errFunction.compute(vecY, vecX)
+    result;
+  }
+
+  def coeffGrad(y: Double, x: Double): Double={
+    var result = 0.
+    result
+  }
+
+}
---
+++ src/main/scala/engine/maths/functions/ScalarFunction.scala
@@ -0,0 +1,33 @@
+package engine.maths.functions
+
+/**
+ * Abstract base class to model scalar functions
+ */
+abstract class ScalarFunction {
+
+  /**
+   * The value of the Polynomial at x
+   */
+  def value(x: Double):Double;
+
+  /**
+   * Compute the gradient of the Polynomial
+   */
+  def getGrad(x: Double): Double = getGrad(x, 1);
+
+  /**
+   * Returns the order-th order gradient
+   */
+  def getGrad(x: Double, order: Integer): Double;
+
+  /**
+   * Returns the gradient with respect to the coefficient
+   */
+  def coeffGrad(x: Double, coeffIdx: Integer): Double;
+
+  /**
+   * Returns the order-th coefficient gradient
+   */
+  def coeffGrad(x: Double, coeffIdx: Integer, order: Integer): Double;
+
+}
