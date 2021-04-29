3c09cab299fec020790bdc2836bbe7a897bafa7d
Alex <a.giavaras@gmail.com>
2021-04-29 17:36:28 +0100

    Add rl worlds

---
+++ src/main/scala/engine/worlds/DiscreteWorld.scala
@@ -0,0 +1,54 @@
+package engine.worlds
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Abstract class for representing
+ * discrete worlds
+ */
+abstract class DiscreteWorld[State, DiscreteActionSpace] {
+
+  /**
+   * The discrete states of the world
+   */
+  val states_ = new ArrayBuffer[State]();
+
+  /**
+   * Returns the number of states in the discrete world
+   * @return
+   */
+  def nStates: Int = states_.length
+
+  /**
+   * Sample an action from the action space
+   * @return
+   */
+  def sampleAction: Int
+
+  /**
+   * Add a new state in the list of states
+   * @param state
+   */
+  def addState(state: State): Unit = {
+
+    if(state == null){
+      throw new NullPointerException("Attempt to add null state")
+    }
+
+    states_ += state
+  }
+
+  /**
+   * Set the sIdx-th state
+   * @param sIdx
+   * @param state
+   */
+  def setState(sIdx: Int, state: State): Unit = {
+
+    if(state == null){
+      throw new NullPointerException("Attempt to add null state")
+    }
+
+    states_(sIdx) = state;
+  }
+
+}
---
+++ src/main/scala/engine/worlds/GridWorld2D.scala
@@ -0,0 +1,12 @@
+package engine.worlds
+import engine.actions.DiscreteActionSpace2D
+
+object GridWorld2D{
+  class State{
+
+  }
+}
+
+class GridWorld2D extends DiscreteWorld[GridWorld2D.State, DiscreteActionSpace2D] {
+
+}
