55397cd53bb302bca388b6bebc63c0f2c847ad32
Alex <a.giavaras@gmail.com>
2021-06-27 12:15:19 +0100

    Update linear regression

--- src/main/scala/examples/regression/LinearRegressionExe_1.scala
+++ src/main/scala/examples/regression/LinearRegressionExe_1.scala
@@ -1,9 +1,11 @@
 package examples.regression
+import breeze.linalg.{DenseMatrix, DenseVector}
 import engine.maths.functions.Polynomial
 import engine.maths.functions.MeanSquaredError
 import engine.maths.functions.LossFunction
 import engine.models.LinearRegression
 import engine.utils.{LineDataSetLoader, SuperVisedTrainerInput, SupervisedTrainer}
+
 
 
 
@@ -15,25 +17,23 @@ object LinearRegressionExe_1 {
     val x = LineDataSetLoader.lineSplit(0.0, 10.0, 100)
 
     System.out.println("Number of training examples: " + x.size)
+
     val coeffs = Array[Double](1.0, 2.0)
     val poly = new Polynomial(coeffs)
     val y = poly.values(x)
 
-    val model = new LinearRegression(coeffs)
-    val errMse = new MeanSquaredError;
-    val loss = new LossFunction(model, errMse)
 
-    val trainingInput = new SuperVisedTrainerInput
-    trainingInput.maxNIterations = 10
-    trainingInput.lossFunction = loss
-    trainingInput.model = model
 
-    // use gradient descent to minimize
-    val trainer = new SupervisedTrainer(trainingInput)
-    trainer.train()
-    //System.out.println("Wins with change choice: " + change_wins)
-    //System.out.println("Probability of winning with initial guess: " + first_choice_wins.asInstanceOf[Double] / N_ITRS)
-    //System.out.println("Probability of winning with change guess: " + change_wins.asInstanceOf[Double] / N_ITRS)
+    // the feature matrix
+    val featureMatrix = DenseMatrix.horzcat(DenseMatrix.ones[Double](x.size, 1), x.toDenseMatrix.t)
+
+    // model
+    val model = new LinearRegression(numFeatures = 1, useIntercept = true)
+
+    model.train(x=featureMatrix,y=y)
+
+    println(s"Polynomial coeffs ${poly.getCoeffsAsDenseVector}")
+    println(s"Linear regressor coeffs ${model.getParameters}")
   }
 
 }
