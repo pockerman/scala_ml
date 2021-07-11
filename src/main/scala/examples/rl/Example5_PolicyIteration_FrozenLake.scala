eee4a338edbbc71ba7d1904de263e2a7f493b644
Alex <a.giavaras@gmail.com>
2021-07-11 17:11:12 +0100

    Update example

--- src/main/scala/examples/rl/Example5_PolicyIteration_FrozenLake.scala
+++ src/main/scala/examples/rl/Example5_PolicyIteration_FrozenLake.scala
@@ -1,5 +1,21 @@
 package examples.rl
 
+import engine.rl.utils.DiscreteUniformPolicy
+import engine.rl.algos.dp.PolicyIteration
+import engine.worlds.FrozenLake
+
+class Agent extends PolicyIteration(environment = new FrozenLake("v0"),
+  nMaxItrs = 100, nPolicyEvalItrs = 100, gamma=1.0, policy=null,
+  policyAdaptor = null, tolerance = 1.0e-7){
+
+}
+
+
 object Example5_PolicyIteration_FrozenLake {
+
+  val environment = new FrozenLake("v0")
+  val policy = DiscreteUniformPolicy(nStates = environment.nStates,
+                                      nActions = environment.nActions prob = null)
+  val policyAdaptor = StochasticAdaptorPolicy()
 
 }
