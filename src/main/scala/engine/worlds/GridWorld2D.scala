41a3f7f9af40956f358d4d84dcd6a4cb7613f316
Alex <a.giavaras@gmail.com>
2021-07-30 14:04:27 +0100

    NO-OP

--- src/main/scala/engine/worlds/GridWorld2D.scala
+++ src/main/scala/engine/worlds/GridWorld2D.scala
@@ -8,5 +8,29 @@ object GridWorld2D{
 }
 
 class GridWorld2D extends DiscreteWorld[GridWorld2D.State, DiscreteActionSpace2D] {
+  /**
+   *
+   * @return
+   */
+  override def nActions: Int = {
+    val actions = 10;
+    actions
+  }
 
+  /**
+   * Sample an action from the action space
+   *
+   * @return
+   */
+  override def sampleAction: Int = {2}
+
+  /**
+   *
+   * @param state
+   * @param action
+   * @return
+   */
+override def getDynamics(state: GridWorld2D.State, action: Int): Array[(Double, GridWorld2D.State, Double, Boolean)] = {
+  null
+}
 }
